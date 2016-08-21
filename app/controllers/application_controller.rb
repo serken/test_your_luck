a5c01b6cba9392034e8cbf06b2073af019460dd0
Pavel Kharchenko <pkharchenko@productengine.com>
2016-08-21 20:22:00 +0300

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   rescue_from Exception do |exception|
-    redirect_to root_path, flash: { danger: exception }
+    redirect_to :back, flash: { danger: exception }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  scope :protocol => 'https://', :constraints => { :protocol => 'https://' } do
+    devise_for :users
+    root to: "home#index"
 
-  get '/profile', to: 'profiles#index'
-  get '/profile/edit', to: 'profiles#edit'
-  get '/profile/show_profile', to: 'profiles#show_profile'
-  get '/profile/wallets', to: 'profiles#wallets'
-  get '/profile/deposit', to: 'profiles#deposit'
-  get '/profile/withdraw', to: 'profiles#withdraw'
-  post '/profile/update_profile', to: 'profiles#update_profile'
+    get '/profile', to: 'profiles#index'
+    get '/profile/edit', to: 'profiles#edit'
+    get '/profile/show_profile', to: 'profiles#show_profile'
+    get '/profile/wallets', to: 'profiles#wallets'
+    get '/profile/deposit', to: 'profiles#deposit'
+    get '/profile/withdraw', to: 'profiles#withdraw'
+    post '/profile/update_profile', to: 'profiles#update_profile'
 
-  post '/profile/make_deposit', to: 'profiles#make_deposit'
-  post '/profile/make_withdraw', to: 'profiles#make_withdraw'
+    post '/profile/make_deposit', to: 'profiles#make_deposit'
+    post '/profile/make_withdraw', to: 'profiles#make_withdraw'
 
-  resources :deals do
-    post 'accept_deal', to: 'deals#accept_deal'
+    resources :deals do
+      post 'accept_deal', to: 'deals#accept_deal'
+    end
   end
 end
